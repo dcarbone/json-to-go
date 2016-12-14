47e098e0c4592b9e12449d05d6fc6b6f22021b67
Daniel Paul Carbone <dcarbone@ena.com>
2016-12-13 21:13:15 -0600

    Explicitly defining SORT_REGULAR as it doesn't seem to actually default to that...

--- src/JSONToGO.php
+++ src/JSONToGO.php
@@ -133,7 +133,7 @@ class JSONToGO
                 {
                     $v = self::sanitizeInput($v);
                 }
-                return array_unique($tmp);
+                return array_unique($tmp, SORT_REGULAR);
 
             case 'object':
                 $tmp = $typeExample;
